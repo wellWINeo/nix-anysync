7d53cf2fc55305309fbe68025e3cbf53c2af840f
wellWINeo <stepan@uspenskiy.su>
2025-05-28 20:22:53 +0300

    Update nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Any-Sync NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs =
